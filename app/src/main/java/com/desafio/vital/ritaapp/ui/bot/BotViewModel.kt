e677664f3c5800508aeb9357ed7c60ad63f83363
Jaqueline Dahmer Steffenon <jaquelinesteffenon@gmail.com>
2020-09-07 21:49:32 -0300

    Rearranges bot and home fragment

--- app/src/main/java/com/desafio/vital/ritaapp/ui/bot/BotViewModel.kt
+++ app/src/main/java/com/desafio/vital/ritaapp/ui/bot/BotViewModel.kt
@@ -1,13 +1,13 @@
-package com.desafio.vital.ritaapp.ui.notifications
+package com.desafio.vital.ritaapp.ui.bot
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class BotViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is bot Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/desafio/vital/ritaapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/desafio/vital/ritaapp/ui/home/HomeFragment.kt
@@ -4,50 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.desafio.vital.ritaapp.R
-import com.tyagiabhinav.dialogflowchatlibrary.Chatbot
-import com.tyagiabhinav.dialogflowchatlibrary.ChatbotActivity
-import com.tyagiabhinav.dialogflowchatlibrary.ChatbotSettings
-import com.tyagiabhinav.dialogflowchatlibrary.DialogflowCredentials
-import java.util.*
-
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-
-        openChatbot(view)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
         return root
     }
-
-    fun openChatbot(view: View?) {
-        // provide your Dialogflow's Google Credential JSON saved under RAW folder in resources
-        DialogflowCredentials.getInstance()
-            .setInputStream(resources.openRawResource(R.raw.dialogflow_credentials))
-        ChatbotSettings.getInstance().chatbot =
-            Chatbot.ChatbotBuilder()
-                .build()
-        val intent =
-            android.content.Intent(context, ChatbotActivity::class.java)
-        val bundle = Bundle()
-
-        // provide a UUID for your session with the Dialogflow agent
-        bundle.putString(ChatbotActivity.SESSION_ID, UUID.randomUUID().toString())
-        intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK or android.content.Intent.FLAG_ACTIVITY_NO_HISTORY)
-        intent.putExtras(bundle)
-        startActivity(intent)
-    }
 }
-
